5ded5125bdeb3d2f20e9c3acb9723e5b03de0ec0
Shiva Reddy <readytohackk@gmail.com>
2019-06-30 10:44:37 +0000

    hover

--- src/PasswordStrengthMeter.js
+++ src/PasswordStrengthMeter.js
@@ -7,10 +7,89 @@ class PasswordStrengthMeter extends Component {
 
 
   render() {
+const {colorBar}=this.props;
+let color={
+    bar1:"gray",
+    bar2:"gray",
+    bar3:"gray",
+    bar4:"gray"
+}
+let barLength=0;
+Object.entries(colorBar).forEach(entry => {
+  if(entry[1]==true){
+      barLength+=1;
+  }
+  //use key and value here
+});
+console.log(barLength);
+switch(barLength){
+case 0:{
+     color={
+    bar1:"gray",
+    bar2:"gray",
+    bar3:"gray",
+    bar4:"gray"
+}
+break;
+}
+case 1:{
+ color={
+    bar1:"red",
+    bar2:"gray",
+    bar3:"gray",
+    bar4:"gray"
+}
+break;
+}
+case 2:{
+ color={
+    bar1:"orange",
+    bar2:"orange",
+    bar3:"gray",
+    bar4:"gray"
+}
+break;
 
+}
+case 3:{
+ color={
+    bar1:"yellow",
+    bar2:"yellow",
+    bar3:"yellow",
+    bar4:"gray"
+}
+break;
+}
+case 4:{
+     color={
+    bar1:"green",
+    bar2:"green",
+    bar3:"green",
+    bar4:"green"
+}
+break;
+
+}
+default:{
+break;
+}
+}
     return (
         <>
       <div>
+          <div className={color.bar1}>
+          test1
+          </div>
+          <div className={color.bar2}>
+          test2
+          </div>
+          <div className={color.bar3}>
+          test3
+          </div>
+          <div className={color.bar4}>
+          test4
+          </div>
+
       </div>
       </>
     );
